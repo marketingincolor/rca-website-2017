c8f383776aaa3d0ee967b845ce7641fd0ff2fcd9
Adam Doe <adoe@marketingincolor.com>
2017-08-22 14:50:51 -0400

    updated breadcrumb alignment

--- themes/rca-inc/template-parts/section-breadcrumbs-social.php
+++ themes/rca-inc/template-parts/section-breadcrumbs-social.php
@@ -5,19 +5,21 @@
 ?>
 
 <div class="row">
-	<div class="small-12 medium-6 columns">
-		<div id="breadcrumbs">
-			<?php if( function_exists('simple_breadcrumb') ) { simple_breadcrumb(); }?>
+	<div class="small-10 small-offset-1">
+		<div class="small-12 medium-6 columns">
+			<div id="breadcrumbs">
+				<?php if( function_exists('simple_breadcrumb') ) { simple_breadcrumb(); }?>
+			</div>
 		</div>
-	</div>
-	<div class="small-12 medium-6 columns text-right">
-		<div id="share">
-			<p style="display:inline-block;">Share on Social Media</p>
-			<a href="#"><i class="fa fa-facebook share-section-btn-color" aria-hidden="true"></i></a>
-			<a href="#"><i class="fa fa-twitter share-section-btn-color" aria-hidden="true"></i></a>
-			<a href="#"><i class="fa fa-linkedin share-section-btn-color" aria-hidden="true"></i></a>
-			<a href="#"><i class="fa fa-envelope share-section-btn-color" aria-hidden="true"></i></a>
-			<a href="#"><i class="fa fa-share share-section-btn-color" aria-hidden="true"></i></a>
+		<div class="small-12 medium-6 columns text-right">
+			<div id="share">
+				<p style="display:inline-block;">Share on Social Media</p>
+				<a href="#"><i class="fa fa-facebook share-section-btn-color" aria-hidden="true"></i></a>
+				<a href="#"><i class="fa fa-twitter share-section-btn-color" aria-hidden="true"></i></a>
+				<a href="#"><i class="fa fa-linkedin share-section-btn-color" aria-hidden="true"></i></a>
+				<a href="#"><i class="fa fa-envelope share-section-btn-color" aria-hidden="true"></i></a>
+				<a href="#"><i class="fa fa-share share-section-btn-color" aria-hidden="true"></i></a>
+			</div>
 		</div>
 	</div>
 </div>
