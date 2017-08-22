c257292e5f87c65b28f73484c5c779f24346cb3e
Adam Doe <adoe@marketingincolor.com>
2017-08-22 14:48:42 -0400

    Removed old file

--- themes/rca-inc/template-parts/section-share-buttons.php
+++
@@ -1,9 +0,0 @@
-<?php ?>
-<div id="share">
-	<p style="display:inline-block;">Share on Social Media</p>
-	<a href="#"><i class="fa fa-facebook share-section-btn-color" aria-hidden="true"></i></a>
-	<a href="#"><i class="fa fa-twitter share-section-btn-color" aria-hidden="true"></i></a>
-	<a href="#"><i class="fa fa-linkedin share-section-btn-color" aria-hidden="true"></i></a>
-	<a href="#"><i class="fa fa-envelope share-section-btn-color" aria-hidden="true"></i></a>
-	<a href="#"><i class="fa fa-share share-section-btn-color" aria-hidden="true"></i></a>
-</div>
